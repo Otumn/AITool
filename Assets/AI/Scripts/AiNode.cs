1ce8fc63c8c6dc0364d2e091ec9c64d14a7345cd
Unknown <nersonnils@gmail.com>
2019-02-02 03:18:28 +0100

    Roll a ball importation, scratchs for actual code

---
+++ Assets/AI/Scripts/AiNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Otumn.Ai
+{
+    public enum NodeType
+    {
+        Input,
+        Condition,
+        Output
+    };
+
+    public class AiNode : MonoBehaviour
+    {
+        private NodeType type;
+        private Dictionary<NodeType, System.Action> activationFunctions;
+    }
+}
